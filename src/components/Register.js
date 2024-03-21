a116405d8d7dfc53baa4c227c11ddbabeb5859f2
Maya T <tettehmaya5@gmail.com>
2024-03-21 15:46:13 +0000

    Completed register form file.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,69 @@
-const Register = ({cakes}) => {
+import { useState } from "react";
+
+const Register = ({cakes, registerCake}) => {
+
+    const [cakeName, setCakeName] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+    const [rating, setRating] = useState("");
+
+    const isValid = () => {
+        let validation =true;
+
+        if(cakeName === "" || ingredients == [] || rating === ""){
+            alert("Fill in all fields please!");
+            validation = false;
+        }
+
+        if(cakes.find(cake => cake.cakeName === cakeName)){
+            alert("This cake already exists!");
+            validation = false;
+        }
+        return validation;
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(isValid()){
+            const newCake = {
+                cakeName,
+                ingredients,
+                rating
+            }
+            registerCake(newCake);
+        }
+    }
+
     return ( 
-        <></>
+        <>
+            <h2>Add a cake:</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="cakeName"
+                    placeholder="Enter cake name"
+                    value={cakeName}
+                    onChange={(event) => setCakeName(event.target.value)}
+                />
+                <input
+                    type="text"
+                    name="ingredients"
+                    placeholder="Enter cake ingredients"
+                    value={ingredients}
+                    onChange={(event) => setIngredients([event.target.value])}
+                />
+                <input
+                    type="number"
+                    min="0"
+                    max="5"
+                    name="rating"
+                    placeholder="Enter cake rating"
+                    value={rating}
+                    onChange={(event) => setRating(event.target.value)}
+                />
+                <input type="submit" value="Submit" />
+            </form>
+        </>
      );
 }
  
